8a8832d2345d0ff327d0474759364a443d114df7
Gene Diaz Jr. <genediazjr@gmail.com>
2015-04-14 22:22:53 +0800

    restore default config

---
+++ config.js
@@ -0,0 +1,39 @@
+
+module.exports = {
+  ssl: {
+    key: '/path/to/your/domain.key',
+    cert: '/path/to/your/domain.crt',
+    enabled: false
+  },
+  server: {
+    port: 8080,
+    root: 'selfie',
+    id: 'selfielog'
+  },
+  aws: {
+    bucket: 'yourBucket',
+    access: 'yourAccessKey',
+    secret: 'yourSecretKey'
+  },
+  imagemin: {
+    interlaced: true,
+    progressive: true,
+    optimizationLevel: 3
+  },
+  hashid: {
+    salt: 'yourHashSalt',
+    length: 4
+  },
+  redis: {
+    port: 0,
+    host: 'your.redis.host'
+  },
+  logger: {
+    info: true,
+    warn: true,
+    debug: true,
+    trace: true,
+    error: true,
+    fatal: true
+  }
+};
